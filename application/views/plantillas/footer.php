cca50310dbd71120e075dcdb9006e24fef2cb2b0
Alex Sullon <alexsullonporras@hotmail.com>
2016-03-04 17:29:51 -0500

    Creación del proyecto
    *- Creación del proyecto de intranet con CodeIgniter

---
+++ application/views/plantillas/footer.php
@@ -0,0 +1,10 @@
+	      	</div>
+	      	<div id="push"></div>
+      	</div>
+		<div id="footer">
+	    	<div class="container">
+	    		<p class="muted credit">&copy; AYMICG - 2013, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+	      	</div>
+	    </div>
+	</body>
+</html>
---
+++ application/views/plantillas/header.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	    <meta charset="utf-8">
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
+	    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+	    <meta name="description" content="">
+	    <meta name="author" content="">
+	    <link rel="icon" href="../../favicon.ico">
+
+	    <title>AYMI S.A.C.</title>
+	    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+	    <style type="text/css">
+	    	html,
+		    body {
+		    	height: 100%;
+	    	}
+
+	      	/* Wrapper for page content to push down footer */
+			#wrap {
+		        min-height: 100%;
+		        height: auto !important;
+		        height: 100%;
+		        /* Negative indent footer by it's height */
+		        margin: 0 auto -60px;
+		    }
+
+			/* Set the fixed height of the footer here */
+			#push,
+			#footer {
+				height: 60px;
+			}
+      		#footer {
+		        background-color: #f5f5f5;
+		    }
+
+      /* Lastly, apply responsive CSS fixes as necessary 
+      @media (max-width: 767px) {
+        #footer {
+          margin-left: -20px;
+          margin-right: -20px;
+          padding-left: 20px;
+          padding-right: 20px;
+        }
+      }*/
+
+
+
+    		/* Custom page CSS
+      		-------------------------------------------------- */
+      		/* Not required for template or sticky footer method. */
+
+     		#wrap > .container {
+		        padding-top: 10px;
+		    }
+      		.container .credit {
+		        margin: 20px 0;
+		    }
+
+      		code {
+		        font-size: 80%;
+		    }
+    	</style>
+	</head>
+	<body>
+		<div id="wrap">
+			<div>
+				<div class="container">
+					<div class="row">
+						<div class="col-md-9">
+							<div class="row">
+								<div class="col-md-9">
+									<img src="img/logo.png" alt="AymiCG">
+								</div>
+							</div>
+						</div>
+						<div class="col-md-3">
+							<form class="navbar-form" role="search">
+						        <div class="input-group">
+						            <input type="text" class="form-control" placeholder="Buscar" name="srch-term" id="srch-term">
+						            <div class="input-group-btn">
+						                <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+						            </div>
+						        </div>
+					        </form>
+						</div>						
+					</div>
+				</div>
+			</div>
+			<nav class="navbar navbar-inverse">
+				<div class="container">
+				    <div class="navbar-header">
+				    	<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+				        	<span class="icon-bar"></span>
+				        	<span class="icon-bar"></span>
+				        	<span class="icon-bar"></span>                        
+				      	</button>
+				      	<a class="navbar-brand" href="#">AYMI S.A.C.</a>
+				    </div>
+			    	<div class="collapse navbar-collapse" id="myNavbar">
+			      		<ul class="nav navbar-nav">
+					        <li class="active"><a href="index.php">Inicio</a></li>
+					        <li class="dropdown">
+					        	<a class="dropdown-toggle" data-toggle="dropdown" href="#">Nosotros<span class="caret"></span></a>
+						        <ul class="dropdown-menu">
+						        	<li><a href="#">Historía</a></li>
+						            <li><a href="#">Misión-Visión</a></li>
+						            <li><a href="#">Objetivos</a></li>
+						        </ul>
+					        </li>
+					        <li class="dropdown">
+					        	<a class="dropdown-toggle" data-toggle="dropdown" href="#">Servicios<span class="caret"></span></a>
+						        <ul class="dropdown-menu">
+						        	<li><a href="#">Aislamiento Acústico</a></li>
+						            <li><a href="#">Aislamiento Térmico</a></li>
+						            <li><a href="#">Carpintería Metálica</a></li>
+						            <li><a href="#">Pintura Industrial</a></li>
+						            <li><a href="#">Venta de Suministros</a></li>
+						        </ul>
+					        </li>
+					        <li><a href="#">Galería</a></li>
+					        <li><a href="#">Clientes</a></li>
+					        <li><a href="#">Contacto</a></li>
+					    </ul>
+		    		</div>
+			  	</div>
+			</nav>
+			<div class="container">
